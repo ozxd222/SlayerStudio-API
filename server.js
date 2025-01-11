243e5c90fe526e2f96717a872b0010e8a2511fd8
ozxd222 <104416629+ozxd222@users.noreply.github.com>
2025-01-11 15:51:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Import routes
-const animeRoutes = require('./src/routes/animeRoutes');
+const animeroutes = require('./src/animeroutes');
 
 // Middleware to handle JSON
 app.use(express.json());
 
 // API routes
-app.use('/anime', animeRoutes);
+app.use('/anime', animeroutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
